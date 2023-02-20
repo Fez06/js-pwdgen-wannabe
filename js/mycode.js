f9be69e1cb8b3785347de6fe3095722cac6e5d51
Fez06 <mattiafezzardi1993@gmail.com>
2023-02-20 15:52:46 +0100

    fixed js

--- js/mycode.js
+++ js/mycode.js
@@ -25,7 +25,7 @@ const prefColor = prompt('Il tuo colore preferito?');
 
 // elaboro la password e faccio un console log
 
-const newPassword = `Ottimo utente (${firstName} ${lastName}), la tua nuova password e' ${firstName}${lastName}${prefColor}89`;
+const newPassword = `Ottimo utente ${firstName} ${lastName}, la tua nuova password e' ${firstName}${lastName}${prefColor}89`;
 
 console.log(newPassword);
 
