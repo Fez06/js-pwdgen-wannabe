e1ad92ba84fd3c013156dc8b89b066a1c82282e7
Fez06 <mattiafezzardi1993@gmail.com>
2023-02-20 15:24:48 +0100

    first commit

---
+++ js/mycode.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const nomeVariabile = "Fez";
+console.log(nomeVariabile);
+
+const anniPersona = '11';
+console.log(nomeVariabile+anniPersona);
+
+const nomeUtente = 'mattia';
+
+
+console.log(nomeUtente);
